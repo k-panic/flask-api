1e06a963617aeefd69d417c14bebcf694f7fd8b4
abarry <abarry>
2020-05-11 03:23:42 +0200

    last push

--- manage.py
+++ manage.py
@@ -1,10 +1,4 @@
 import requests
-import subprocess
-
-p = subprocess.run("python app.py", capture_output=True)
-assert p.returncode == 0
-
-print('the app is running successfully, trying the test request ...')
 
 url = "http://localhost:3000/test"
 
